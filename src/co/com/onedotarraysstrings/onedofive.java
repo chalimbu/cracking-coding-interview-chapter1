4c628607a10209a02057a185995e9de87d2f0382
Sebastian Zapata <sebastian.zapata@geniussports.com>
2022-03-29 17:08:31 -0500

    enunciado del ejercisio 1.5

---
+++ src/co/com/onedotarraysstrings/onedofive.java
@@ -0,0 +1,20 @@
+package co.com.onedotarraysstrings;
+
+public class onedofive {
+    /*
+    * One Away: There are thre types of edits that can be performed on strings: insert a character, remove
+    * a character, or replace a character. Given two strings, write a function to check if they are one edit
+    * (or zero edits) away
+    *
+    * example
+    * pale,ple -> true
+    * pales, pale -> true
+    * pale, bale -> true
+    * pale, bake -> false
+    *
+    * */
+
+
+    public static void main(String[] args) {
+    }
+}
