290e20a503a1fb50f0753e5cc408eba3fd00d84b
Sebastian Zapata <sebastian.zapata@geniussports.com>
2022-03-29 17:28:55 -0500

    codigo para la solucion del ejercisio 5

--- src/co/com/onedotarraysstrings/onedofive.java
+++ src/co/com/onedotarraysstrings/onedofive.java
@@ -16,5 +16,56 @@ public class onedofive {
 
 
     public static void main(String[] args) {
+        System.out.println(oneAway("pale","ple"));
+        System.out.println(oneAway("pales","pale"));
+        System.out.println(oneAway("pale","bale"));
+        System.out.println(oneAway("pale","bake"));
+    }
+
+    private static boolean oneAway(String str1, String str2) {
+        String largest=str1.length()>=str2.length() ? str1 : str2;
+        String smallest=str1.length()>=str2.length() ? str2 : str1;
+        if((largest.length()-smallest.length())>1){
+            return false;
+        }
+        if(largest.length()==smallest.length()){
+            return comparasionSameLength(largest,smallest);
+        }else{
+            return comparasionDifferentLength(largest,smallest);
+        }
+    }
+
+    private static boolean comparasionDifferentLength(String largest, String smalles) {
+        boolean different=false;
+        int indexSmalles=0;
+        int indexBigger=0;
+        while(indexSmalles<smalles.length() && indexBigger<largest.length()){
+
+            if(largest.charAt(indexBigger)!=smalles.charAt(indexSmalles)){
+                if(indexBigger!=indexSmalles || different){
+                    return false;
+                }
+                different=true;
+                indexBigger++;
+            }else{
+                indexBigger++;
+                indexSmalles++;
+            }
+        }
+
+        return true;
+    }
+
+    private static boolean comparasionSameLength(String largest, String smalles) {
+        boolean different=false;
+        for(int i=0;i<largest.length();i++){
+            if(largest.charAt(i)!=smalles.charAt(i)){
+                if(different){
+                    return false;
+                }
+                different=true;
+            }
+        }
+        return true;
     }
 }
