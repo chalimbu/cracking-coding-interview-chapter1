90caeae33978331786035c9d726d7dbab87d4f6f
Sebastian Zapata <sebastian.zapata@geniussports.com>
2022-06-30 11:21:05 -0500

    cambiando solucion por una mas sencilla rapida

--- src/co/com/onedotarraysstrings/onedofive.java
+++ src/co/com/onedotarraysstrings/onedofive.java
@@ -15,57 +15,17 @@ public class onedofive {
     * */
 
 
-    public static void main(String[] args) {
-        System.out.println(oneAway("pale","ple"));
-        System.out.println(oneAway("pales","pale"));
-        System.out.println(oneAway("pale","bale"));
-        System.out.println(oneAway("pale","bake"));
+    public static void main(String args[]){
+        System.out.println(isRotation("waterbotle","erbotlewat"));//true
+        System.out.println(isRotation("waterbotle","erbotlewate"));//false
+        System.out.println(isRotation("waterbotle","erbetlewat"));//false
+        System.out.println(isRotation("aaabbcc","abbccaa"));//true
+
     }
 
-    private static boolean oneAway(String str1, String str2) {
-        String largest=str1.length()>=str2.length() ? str1 : str2;
-        String smallest=str1.length()>=str2.length() ? str2 : str1;
-        if((largest.length()-smallest.length())>1){
-            return false;
-        }
-        if(largest.length()==smallest.length()){
-            return comparasionSameLength(largest,smallest);
-        }else{
-            return comparasionDifferentLength(largest,smallest);
-        }
-    }
-
-    private static boolean comparasionDifferentLength(String largest, String smalles) {
-        boolean different=false;
-        int indexSmalles=0;
-        int indexBigger=0;
-        while(indexSmalles<smalles.length() && indexBigger<largest.length()){
-
-            if(largest.charAt(indexBigger)!=smalles.charAt(indexSmalles)){
-                if(indexBigger!=indexSmalles || different){
-                    return false;
-                }
-                different=true;
-                indexBigger++;
-            }else{
-                indexBigger++;
-                indexSmalles++;
-            }
-        }
-
-        return true;
-    }
-
-    private static boolean comparasionSameLength(String largest, String smalles) {
-        boolean different=false;
-        for(int i=0;i<largest.length();i++){
-            if(largest.charAt(i)!=smalles.charAt(i)){
-                if(different){
-                    return false;
-                }
-                different=true;
-            }
-        }
-        return true;
+    public static boolean isRotation(String s1,String s2){
+        if(s1.length()!=s2.length()) return false;
+        String concatenate=s1+s1;
+        return concatenate.contains(s2);
     }
 }
